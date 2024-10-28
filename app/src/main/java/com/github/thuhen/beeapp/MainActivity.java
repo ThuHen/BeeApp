55790f1a7dfe05a47cf9a83ec2b21ac3cf91bedb
ThuHen <lehen7a2@gmail.com>
2024-10-28 14:37:42 +0700

    thêm 2 nút button
    Signed-off-by: ThuHen <lehen7a2@gmail.com>

---
+++ app/src/main/java/com/github/thuhen/beeapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.github.thuhen.beeapp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+private Button mDriver, mCustomer;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        mDriver=(Button) findViewById(R.id.driver);
+        mCustomer=(Button) findViewById(R.id.customer);
+
+    }
+}

bdb2f38c3dd5375f374c1f825b04561c16382687
Hoang Hue <hoanghue26804@gmail.com>
2024-12-24 21:45:34 +0700

    video 30 chua chay

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,9 @@ dependencies {
     // https://firebase.google.com/docs/firestore/solutions/geoqueries
     implementation ("com.firebase:geofire-android-common:3.2.0")
 //    implementation ("com.google.android.gms:play-services-location:21.0.1")
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
+
 
 
 
--- app/src/main/java/com/github/thuhen/beeapp/HistorySingleActivity.java
+++ app/src/main/java/com/github/thuhen/beeapp/HistorySingleActivity.java
@@ -1,6 +1,10 @@
 package com.github.thuhen.beeapp;
 
+import android.media.AudioRouting;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -12,21 +16,141 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.bumptech.glide.Glide;
+
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
 
 public class HistorySingleActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private String rideId, currentUserId, customerId, driverId, userDriverOrCustomer;
+
+    private TextView rideLocation;
+    private TextView rideDistance;
+    private TextView rideDate;
+    private TextView userName;
+    private TextView userPhone;
+
+    private ImageView userImage;
+
+    private DatabaseReference historyRideInfoDb;
+    private LatLng destinationLatLng, pickupLatLng;
     private GoogleMap mMap;
     private SupportMapFragment mMapFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_single);
+
+        rideId = getIntent().getExtras().getString("rideId");
+
         mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
         mMapFragment.getMapAsync(this);
+
+        rideLocation = (TextView) findViewById(R.id.rideLocation);
+        rideDistance = (TextView) findViewById(R.id.rideDistance);
+        rideDate = (TextView) findViewById(R.id.rideDate);
+        userName = (TextView) findViewById(R.id.userName);
+        userPhone = (TextView) findViewById(R.id.userPhone);
+
+        userImage = (ImageView) findViewById(R.id.userImage);
+
+        currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        historyRideInfoDb = FirebaseDatabase.getInstance().getReference().child("history").child(rideId);
+
+        getRideInformation();
     }
 
+    private void getRideInformation() {
+        historyRideInfoDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot child:snapshot.getChildren()){
+                        if(child.getKey().equals("customer")){
+                            customerId= child.getValue().toString();
+                            if(!customerId.equals(currentUserId)){
+                                userDriverOrCustomer = "Drivers";
+                                getUserInformation("Customers", customerId);
+                            }
+                        }
+                        if(child.getKey().equals("driver")){
+                            driverId= child.getValue().toString();
+                            if(!driverId.equals(currentUserId)){
+                                userDriverOrCustomer = "Customers";
+                                getUserInformation("Drivers", driverId);
+                            }
+                        }
+                        if(child.getKey().equals("timestamp")){
+                            rideDate.setText(getDate(Long.valueOf(child.getValue().toString())));
+                        }
+                        if(child.getKey().equals("destination")){
+                            rideLocation.setText(getDate(Long.valueOf(child.getValue().toString())));
+                        }
+                        if(child.getKey().equals("destination")){
+                            pickupLatLng = new LatLng(Double.valueOf(child.child("from").child("lat").getValue().toString()), Double.valueOf(child.child("from").child("lng").getValue().toString()));
+                            destinationLatLng = new LatLng(Double.valueOf(child.child("to").child("lat").getValue().toString()), Double.valueOf(child.child("to").child("lng").getValue().toString()));
+//                            if(destinationLatLng!=new LatLng(0,0)){
+//
+//                            }
+
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getUserInformation(String otherUserDriverOrCustomer, String otherUserId) {
+        DatabaseReference mOtherUserDB = FirebaseDatabase.getInstance().getReference().child("Users").child(otherUserDriverOrCustomer).child(otherUserId);
+        mOtherUserDB.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    Map<String, Object> map = (Map<String, Object>) snapshot.getValue();
+                    if(map.get("name")!=null){
+                        userName.setText(map.get("name").toString());
+                    }
+                    if(map.get("phone")!=null){
+                        userName.setText(map.get("phone").toString());
+                    }
+                    if(map.get("profileImageUrl")!=null){
+                        Glide.with(getApplication()).load(map.get("profileImageUrl").toString()).into(userImage);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private String getDate(Long timestamp){
+        Calendar cal= Calendar.getInstance(Locale.getDefault());
+        cal.setTimeInMillis(timestamp*1000);
+        String date = DateFormat.format("dd-MM-yyyy hh:mm",cal).toString();
+        return date;
+    }
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-
+        mMap=googleMap;
     }
+
 }
